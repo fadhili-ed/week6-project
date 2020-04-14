c068f64fda9cff22238ee240a64553c619792936
fadhili-ed <fadhilied00@gmail.com>
2020-04-14 14:13:55 +0300

    updates on cypress simple test to check if the deposits page has the word Deposits

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,6 +2,9 @@ describe('Deposits Page', () => {
     it('successfully loads', () => {
         cy.visit('http://localhost:9292/deposits')
         cy.contains('Deposits')
+<<<<<<< HEAD
         cy.get('#deposits_form').should('contain', 'submit')
+=======
+>>>>>>> b102893... updates on cypress simple test to check if the deposits page has the word Deposits
     })
 })
