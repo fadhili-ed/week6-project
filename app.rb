995fa8b0b4ab52336492ffedcdfd12bef10b6309
fadhili-ed <fadhilied00@gmail.com>
2020-04-15 11:00:40 +0300

    practice on basic cypress commands

--- app.rb
+++ app.rb
@@ -3,10 +3,7 @@ require 'sinatra'
 get '/' do 
     slim :home
 end
-<<<<<<< HEAD
 
 get '/deposits' do
     slim :deposits
 end
-=======
->>>>>>> 400be4d... My portfolio, the initial homepage
--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,6 +2,14 @@ describe('Deposits Page', () => {
     it('successfully loads', () => {
         cy.visit('http://localhost:9292/deposits')
         cy.contains('Deposits')
-        cy.get('#deposits_form').should('contain', 'submit')
+        // check if there is a form
+        cy.get('form')
+        // check if the form has the desired class
+        cy.get('form').should('have.class', 'deposits_form')
+        // typing into a text box
+        cy.get('input:first')
+          .type('Jon')
+        cy.get('input:last')  
+          .type('70000')
     })
 })
